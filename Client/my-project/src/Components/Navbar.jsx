492e5b105c4c64ea0f453406a33699a28860e89b
deepakchauhan33 <deep651965@gmail.com>
2026-09-12 17:55:39 +0530

    Update import

--- Client/my-project/src/Components/Navbar.jsx
+++ Client/my-project/src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ import { motion } from "framer-motion";
 
 
 // Wishlist Service 
-import { useGetWishlistQuery } from "../Pages/Wishlist/wishlistApi";
+import { useGetWishlistQuery } from "../Pages/Wishlist/wishlistAPI";
 
 
 // Cart Service
